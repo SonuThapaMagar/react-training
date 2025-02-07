6531270a28c8bd10f95e588b38dd9925e8f998b8
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-07 07:54:44 +0545

    React router installed and readme file changed

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+
+## Environment setup
+- Install Node js
+- Install VS Code
+
+## Install React
+- `npx create-react-app react-training`
+- `cd react-training`
+- `npm start`
+
 ## How to start project ?
 - Open vs code
 - cd project(react-training)
--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './assets/css/main.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Dashboard from './pages/dashboard';
 
 const App = () => {
   return (
     <div>
+     <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Users />} />
+      <Route path="/" element={<Setting />} />
+    </Routes>
       <Header />
       <div className="main_wrapper">
         <Sidebar />
