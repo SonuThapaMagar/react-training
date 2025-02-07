4c85de1e83daf664c27128f3af82f594738176cb
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-07 08:21:43 +0545

    Pages navigated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { NavLink } from "react-router";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="menu">
                 <ul className='menus'>
-                    <li className='menu-items'>Dashboard</li>
-                    <li className='menu-items'>Users</li>
-                    <li className='menu-items'>Setting</li>
+                    <li className='menu-items'>
+                        <NavLink to="/dashboard" end>
+                            Dashboard
+                        </NavLink>
+                    </li>
+                    <li className='menu-items'>
+                        <NavLink to="/users" end>
+                            Users
+                        </NavLink>
+                    </li>
+                    <li className='menu-items'>
+                        <NavLink to="/setting" end>
+                            Setting
+                        </NavLink></li>
                     <li className='menu-items'>Logout</li>
                 </ul>
             </div>
