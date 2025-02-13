94a507172fc34c3b7f5f9786ce54a9210a9d0f34
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-13 06:15:46 +0545

    Icon added in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,42 @@
 import React from 'react'
 import { NavLink } from "react-router";
 import { MdDashboard } from "react-icons/md";
-// import { FaUser } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { IoSettings } from "react-icons/io5";
+import { IoIosLogOut } from "react-icons/io";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <ul>
                 <li className='menu-items'>
-                <NavLink to="/dashboard" className={({ isActive }) => (isActive ? 'active' : '')} end>
-                        <MdDashboard />
-                        Dashboard
+                    <NavLink to="/dashboard" className={({ isActive }) => (isActive ? 'active' : '')} end>
+                        <span>
+                            <MdDashboard />
+                            Dashboard
+                        </span>
                     </NavLink>
                 </li>
                 <li className='menu-items'>
-                    <NavLink to="/users" end>
-                        Users
+                    <NavLink to="/users" className={({ isActive }) => (isActive ? 'active' : '')} end>
+                        <span>
+                            <FaUser />
+                            Users
+                        </span>
                     </NavLink>
                 </li>
                 <li className='menu-items'>
-                    <NavLink to="/setting" end>
-                        Setting
+                    <NavLink to="/setting" className={({ isActive }) => (isActive ? 'active' : '')} end>
+                        <span>
+                            <IoSettings />
+                            Setting
+                        </span>
                     </NavLink></li>
                 <li className='menu-items'>
-                    Logout</li>
+                    <span>
+                        <IoIosLogOut />
+                        Logout
+                    </span></li>
             </ul>
         </div>
     )
