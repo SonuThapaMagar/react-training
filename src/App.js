1ecf2e9fd267a23b66d0491bce2540845caf8342
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-14 08:10:15 +0545

    List of users displayed using map function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './assets/css/main.css';
 import './assets/css/sidebar.css';
+import './assets/css/table.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Footer from './components/Footer.js';
--- src/pages/user/Users.js
+++ src/pages/user/Users.js
@@ -1,9 +1,73 @@
 import React from 'react'
 
+
 const users = (props) => {
+  const users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      age: 25,
+      email: 'abc@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      age: 23,
+      email: 'jane@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'John Smith',
+      age: 30,
+      email: 'john@gmail.com',
+    },
+    {
+      id: 4,
+      name: 'Jane Smith',
+      age: 28,
+      email: 'smith@gmail.com'
+    },
+    {
+      id: 5,
+      name: 'John Doe',
+      age: 25,
+      email: 'doe@gmail.com',
+    }
+  ];
   return (
     <div>
       {props.title}
+      <table class="styled-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.age}</td>
+                  <td>{user.email}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+      {/* {
+        users.map((user) => {
+          return (
+            <div>{user.name}</div>
+          );
+        })
+      } */}
     </div>
   )
 }
