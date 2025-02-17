e955266e05fc289fcf898cdae76fa64210c82457
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-17 08:17:31 +0545

    useState hook for all input field added

--- src/pages/user/AddUser.js
+++ src/pages/user/AddUser.js
@@ -2,12 +2,20 @@ import React,{useState} from 'react'
 
 const AddUser = () => {
   const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
 
   const handleSave = () => {
     // save user
   }
   const handleNameChange = (e) => {
     setName(e.target.value);
+  }
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  }
+  const handleEmailChange = (e) => {  
+    setEmail(e.target.value);
   }
   return (
     <div className="form-container">
@@ -17,10 +25,10 @@ const AddUser = () => {
         <input type="text" id="name" name="name" value={name} onChange={handleNameChange} placeholder="Enter full name" />
 
         <label htmlFor="age">Age</label>
-        <input type="number" id="age" name="age" placeholder="Enter your age" />
+        <input type="number" id="age" name="age" value={age} onChange={handleAgeChange} placeholder="Enter your age" />
         
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="Enter your email" />
+        <input type="email" id="email" name="email"  value={email} onChange={handleEmailChange} placeholder="Enter your email" />
 
         <button type="button" onClick={handleSave} className="btn-save">Save</button>
       </form>
