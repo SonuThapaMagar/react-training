e906852247e5165bec69831648615d9d4f5e58f8
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-17 08:10:00 +0545

    useState hook for state management

--- src/pages/user/AddUser.js
+++ src/pages/user/AddUser.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const AddUser = () => {
+  const [name, setName] = useState('');
+
   const handleSave = () => {
     // save user
+  }
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   }
   return (
     <div className="form-container">
     <h2>Add User</h2>
       <form className="form-group">
         <label htmlFor="name">Full Name</label>
-        <input type="text" id="name" name="name" placeholder="Enter full name" />
+        <input type="text" id="name" name="name" value={name} onChange={handleNameChange} placeholder="Enter full name" />
 
         <label htmlFor="age">Age</label>
         <input type="number" id="age" name="age" placeholder="Enter your age" />
