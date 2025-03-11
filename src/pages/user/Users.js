4c0ca807b5dcfa8323d7ed92a700afef17400e70
SonuThapaMagar <sonumagar787@gmail.com>
2025-03-11 15:20:48 +0545

    Ant design table used

--- src/pages/user/Users.js
+++ src/pages/user/Users.js
@@ -1,16 +1,45 @@
 import React from 'react'
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import UserHeader from './UserHeader';
-import UserRow from './UserRow';
+import { Table, Button, Radio, Typography } from 'antd';
+import { UserAddOutlined } from '@ant-design/icons';
 
 const Users = (props) => {
 
   const navigate = useNavigate();
-
+  const [size, setSize] = useState('large');
   const [data, setData] = useState([
 
   ]);
+
+  const columns = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Action',
+      dataIndex: '',
+      key: 'x',
+      render: () => <a>Delete</a>,
+    },
+  ];
 
   useEffect(() => {
     setData(
@@ -53,23 +82,36 @@ const Users = (props) => {
     navigate("addUser");
   }
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <button className="addUser" onClick={handleAddUser}>Add User
-      </button>
-      <table className="styled-table">
-        <thead>
-          <UserHeader />
-        </thead>
-        <tbody>
-          {
-            data.map((user) => (
-              <UserRow item={user}
-              />
-            ))
-          }
-        </tbody>
-      </table>
+    <div style={{ padding: '20px' }}>
+      <Typography.Title editable level={1} style={{ margin: 8 }}>
+        Users
+      </Typography.Title>
+      <Button
+        type="primary"
+        shape="round"
+        icon={<UserAddOutlined />}
+        size={size}
+        onClick={handleAddUser}
+        style={{ margin: 8 }}
+      >
+        Add User
+      </Button>
+      <Table
+        columns={columns}
+        expandable={{
+          expandedRowRender: (record) => (
+            <p
+              style={{
+                margin: 0,
+              }}
+            >
+              {record.description}
+            </p>
+          ),
+          rowExpandable: (record) => record.name !== 'Not Expandable',
+        }}
+        dataSource={data}
+      />
     </div>
   )
 }
